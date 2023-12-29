c7bab25bef40ad08ec647347a0268291c9236652
btsutskiridze <bakartsutskiridze@redberry.ge>
2023-12-29 18:57:26 +0400

    feat: add user model

---
+++ src/main/java/com/bakari/servletsdemo/entity/User.java
@@ -0,0 +1,46 @@
+package com.bakari.servletsdemo.entity;
+
+public class User {
+
+    private long id;
+    private String username;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public User(long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
